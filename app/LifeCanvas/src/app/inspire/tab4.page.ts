54f3df8f3e1596ac38fd3d590bcace2a8b040303
Nadine <n4dinevid4l@hotmail.com>
2023-12-22 21:01:26 +0100

    Functional Segments in inspire and planner tabs

--- app/LifeCanvas/src/app/inspire/tab4.page.ts
+++ app/LifeCanvas/src/app/inspire/tab4.page.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
-
+  inspireSegment = 'affirmations';
   constructor() { }
 
   ngOnInit() {
