3d93de3dcb298abcd509c4b9e84699069cd2a670
Nadine <n4dinevid4l@hotmail.com>
2023-12-24 16:39:56 +0100

    Login Page - Set

--- app/LifeCanvas/src/app/app-routing.module.ts
+++ app/LifeCanvas/src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
@@ -9,7 +14,8 @@ const routes: Routes = [
   {
     path: 'tab4',
     loadChildren: () => import('./inspire/tab4.module').then( m => m.Tab4PageModule)
-  }
+  },
+
 
 ];
 @NgModule({
