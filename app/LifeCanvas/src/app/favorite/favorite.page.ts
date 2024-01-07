30fe6a12767136f06c951a8e7a90d82a812a72a5
Nadine <n4dinevid4l@hotmail.com>
2024-01-07 15:30:42 +0100

    Comments in favorite page, html and ts

--- app/LifeCanvas/src/app/favorite/favorite.page.ts
+++ app/LifeCanvas/src/app/favorite/favorite.page.ts
@@ -1,4 +1,13 @@
+/**
+ * Represents a component that is initialized when the Angular application starts.
+ * @class
+ * @implements {OnInit}
+ */
 import { Component, OnInit } from '@angular/core';
+/**
+ * Imports the FileService from the '../service/file.service' module.
+ * @returns The FileService object.
+ */
 import { FileService } from '../service/file.service';
 
 @Component({
@@ -6,18 +15,48 @@ import { FileService } from '../service/file.service';
   templateUrl: './favorite.page.html',
   styleUrls: ['./favorite.page.scss'],
 })
+/**
+ * Represents a favorite page component that displays quotes and affirmations.
+ * @class FavoritePage
+ * @implements {OnInit}
+ */
 export class FavoritePage implements OnInit {
+  /**
+   * The segment that is currently being inspired. 
+   * @type {string}
+   */
   inspireSegment = 'affirmations';
+  /**
+   * The quotes property of an object.
+   * @type {any | undefined}
+   */
   quotes: any | undefined;
+  /**
+   * An optional property that represents a collection of affirmations.
+   * @type {any | undefined}
+   */
   affirmations: any | undefined;
 
+  /**
+   * Creates a new instance of the class.
+   * @param {FileService} fileService - The file service used by the class.
+   */
   constructor(private fileService: FileService) { }
 
+  /**
+   * Initializes the component by reading affirmations and quotes from the file service.
+   * @returns None
+   */
   async ngOnInit() {
     this.affirmations = await this.fileService.readAffirmations();
     this.quotes = await this.fileService.readQuotes();
   }
 
+  /**
+   * Deletes a quote from the file service and updates the list of quotes.
+   * @param {any} quote - The quote to be deleted.
+   * @returns None
+   */
   deleteQuote(quote: any) {
     this.fileService.deleteQuotes(quote).then(() => 
       this.fileService.readQuotes().then((value) => {
@@ -25,6 +64,11 @@ export class FavoritePage implements OnInit {
         }));
   }
 
+  /**
+   * Deletes an affirmation from the file service and updates the list of affirmations.
+   * @param {any} affirmation - The affirmation to delete.
+   * @returns None
+   */
   deleteAffirmation(affirmation: any) {
     this.fileService.deleteAffirmation(affirmation.quote).then(() => this.fileService.readAffirmations().then((value) => {
        this.affirmations = value;
