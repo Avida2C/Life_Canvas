ff25dae41b28303ba4072638f45a76e4a85b266a
Nadine <n4dinevid4l@hotmail.com>
2024-01-07 16:41:16 +0100

    Comments: Tabs pages - ts and html

--- app/LifeCanvas/src/app/tabs/tabs.page.ts
+++ app/LifeCanvas/src/app/tabs/tabs.page.ts
@@ -1,3 +1,11 @@
+/**
+ * Import necessary modules and dependencies for an Angular component.
+ * @module Angular
+ * @component Component
+ * @implements OnInit
+ * @param {Router} Router - The Angular router module.
+ * @param {Preferences} Preferences - The Capacitor preferences module.
+ */
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Preferences } from '@capacitor/preferences';
@@ -9,14 +17,32 @@ import { Preferences } from '@capacitor/preferences';
 })
 export class TabsPage implements OnInit {
 
+  /**
+   * Represents the state of the theme toggle and the name property.
+   * @property {boolean} themeToggle - Indicates whether the theme is toggled on or off.
+   * @property {any} name - The name property.
+   */
   themeToggle = false;
   name: any;
+
+  /**
+   * Constructs a new instance of the class.
+   * @param {Router} router - The router object used for navigation.
+   */
   constructor(private router: Router) {}
 
+  /**
+   * Initializes the component by retrieving preferences and setting up the dark theme.
+   * @returns None
+   */
   ngOnInit() {
     // Use matchMedia to check the user preference
     Preferences.get({key: 'darkmode'}).then(({value}) => {
       let prefersDark;
+      /**
+       * Initializes the dark theme based on the given value or the user's preference.
+       * @param {boolean | null} value - The value indicating whether to use the dark theme. If null, the user's preference will be used.
+       */
       if(value == null) {
         let prefersDarkMedia = window.matchMedia('(prefers-color-scheme: dark)');
         prefersDarkMedia.addEventListener('change', (mediaQuery) => this.initializeDarkTheme(mediaQuery.matches));
@@ -30,23 +56,39 @@ export class TabsPage implements OnInit {
       this.initializeDarkTheme(prefersDark);
     });
 
+    /**
+     * Retrieves the value of the 'name' key from the Preferences storage and assigns it to the 'name' property of the current object.
+     */
     Preferences.get({key: 'name'}).then(({value}) => {
       this.name = value;
     });
     
   }
 
+  /**
+   * Initializes the dark theme based on the provided value.
+   * @param {any} isDark - The value indicating whether the dark theme should be enabled.
+   * @returns None
+   */
   initializeDarkTheme(isDark: any) {
     this.themeToggle = isDark;
     this.toggleDarkTheme(isDark);
   }
 
-  // Listen for the toggle check/uncheck to toggle the dark theme
+  /**
+   * Toggles the dark theme based on the value of the checkbox.
+   * @param {any} ev - The event object containing the checkbox value.
+   * @returns None
+   */
   toggleChange(ev: any) {
     this.toggleDarkTheme(ev.detail.checked);
   }
 
-  // Add or remove the "dark" class on the document body
+  /**
+   * Toggles the dark theme on or off based on the provided boolean value.
+   * @param {boolean} shouldAdd - Indicates whether the dark theme should be added (true) or removed (false).
+   * @returns None
+   */
   async toggleDarkTheme(shouldAdd: boolean) {
     document.body.classList.toggle('dark', shouldAdd);
     Preferences.set({
@@ -55,6 +97,10 @@ export class TabsPage implements OnInit {
     });
   }
 
+  /**
+   * Saves the name value to the preferences and navigates to the tab1 page with the updated name parameter.
+   * @returns None
+   */
   saveName() {
     Preferences.set({
       key: 'name',
@@ -64,16 +110,29 @@ export class TabsPage implements OnInit {
     });
   }
 
+  /**
+   * Opens the editor page based on the given type.
+   * @param {string} type - The type of editor to open.
+   * @returns None
+   */
   openEditor(type:string)
   {
     this.router.navigateByUrl('/editor?type=' + type);
   }
 
+  /**
+   * Opens the task editor page by navigating to the '/task-editor' URL.
+   * @returns None
+   */
   openTaskEditor()
   {
     this.router.navigateByUrl('/task-editor');
   }
 
+  /**
+   * Navigates to the "Favorite" page in the application.
+   * @returns None
+   */
   openFavorites(){
     this.router.navigate(['/tabs/favorite']);
   }
