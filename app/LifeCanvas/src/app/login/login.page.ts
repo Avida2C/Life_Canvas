2fa8a5db7fb8e1c75d63a9c4cb2ab156529dc3d4
Nadine <n4dinevid4l@hotmail.com>
2024-01-07 15:42:50 +0100

    Comments: Login Page - ts and html

--- app/LifeCanvas/src/app/login/login.page.ts
+++ app/LifeCanvas/src/app/login/login.page.ts
@@ -1,3 +1,15 @@
+/**
+ * Imports necessary modules and dependencies for an Angular component.
+ * @module AngularComponentImports
+ * @requires Component
+ * @requires OnInit
+ * @requires FormBuilder
+ * @requires FormControl
+ * @requires FormGroup
+ * @requires Validators
+ * @requires Router
+ * @requires Preferences
+ */
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -10,23 +22,45 @@ import { Preferences } from '@capacitor/preferences';
 })
 export class LoginPage implements OnInit {
 
+  /**
+   * Represents a form group for login functionality.
+   * @type {FormGroup}
+   */
   formLogin: FormGroup;
   
-  constructor(private fb: FormBuilder, private route: Router) { 
-    
-  }
+  /**
+   * Constructs a new instance of the class.
+   * @param {FormBuilder} fb - The FormBuilder instance used for creating forms.
+   * @param {Router} route - The Router instance used for navigating between routes.
+   */
+  constructor(private fb: FormBuilder, private route: Router) {}
 
+  /**
+   * Navigates to the next page after setting the user's name in the preferences.
+   * @returns None
+   */
   nextpage() {
     let userName = this.formLogin.get('name')?.value;
+    /**
+     * Sets the user's name in the preferences and navigates to the '/tabs' route.
+     * @param {string} userName - The user's name to be stored in the preferences.
+     * @returns None
+     */
     Preferences.set({
       key: 'name',
       value: userName,
     }).then(() => {
       this.route.navigate(['/tabs']);
     });
-    
   }
 
+  /**
+   * Initializes the component and performs necessary setup tasks.
+   * - Retrieves the value of the 'name' key from the Preferences storage.
+   * - If a value is found, logs it to the console and navigates to the '/tabs' route.
+   * - Sets up the formLogin FormGroup with a 'name' field that is required.
+   * @returns None
+   */
   ngOnInit() {
     Preferences.get({ key: 'name' }).then(({value}) => {
       if(value) {
@@ -39,5 +73,4 @@ export class LoginPage implements OnInit {
       name: ['Name', [Validators.required]]
     });
   }
-
 }
