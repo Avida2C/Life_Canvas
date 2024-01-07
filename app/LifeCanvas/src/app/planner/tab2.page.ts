c7d502b5f71d138d7d8097cc6750f5e08ec871e0
Nadine <n4dinevid4l@hotmail.com>
2024-01-07 16:02:44 +0100

    Comments: Planner - ts and html

--- app/LifeCanvas/src/app/planner/tab2.page.ts
+++ app/LifeCanvas/src/app/planner/tab2.page.ts
@@ -1,6 +1,23 @@
+/**
+ * Represents an Angular component that implements the OnInit interface.
+ * @module @angular/core
+ * @class Component
+ * @implements OnInit
+ */
 import { Component, OnInit } from '@angular/core';
+/**
+ * Imports the FileService from the '../service/file.service' module.
+ * @returns The FileService object.
+ */
 import { FileService } from '../service/file.service';
+/**
+ * Import the `ActivatedRoute` and `Router` modules from the `@angular/router` package.
+ */
 import { ActivatedRoute, Router } from '@angular/router';
+/**
+ * Import the entire underscore library and assign it to the variable '_'.
+ * This allows you to use all the functions provided by the underscore library.
+ */
 import * as _ from 'underscore';
 
 @Component({
@@ -9,15 +26,39 @@ import * as _ from 'underscore';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page implements OnInit {
-  public plannerSegment = 'calendar';
 
+  /**
+   * The current segment of the planner.
+   * @type {string}
+   */
+  public plannerSegment = 'calendar';
+  /**
+   * A promise that resolves to any value.
+   * @returns A promise that resolves to any value.
+   */
   public notes: Promise<any>;
+  /**
+   * The tasks property of the class.
+   * @type {any}
+   */
   public tasks: any;
   
-  constructor(private fileService: FileService, private router: Router, private route: ActivatedRoute) {
+  /**
+   * Constructs a new instance of the class.
+   * @param {FileService} fileService - The file service used for file operations.
+   * @param {Router} router - The router used for navigation.
+   * @param {ActivatedRoute} route - The route used for accessing route parameters.
+   */
+  constructor(private fileService: FileService, private router: Router, private route: ActivatedRoute) {}
 
-  }
-
+  /**
+   * Initializes the component and subscribes to the queryParamMap of the route.
+   * Retrieves the 'open' parameter from the query parameters and assigns it to the 'plannerSegment' property if it exists.
+   * Reads the notes from the file service and assigns them to the 'notes' property.
+   * Reads the tasks from the file service and assigns them to the 'tasks' property.
+   * Logs the retrieved tasks to the console.
+   * @returns None
+   */
   ngOnInit() {
     this.route.queryParamMap.subscribe(params => {
       const segment = params.get('open');
@@ -30,25 +71,52 @@ export class Tab2Page implements OnInit {
     });
   }
 
+  /**
+   * Opens a note with the specified ID.
+   * @param {string} id - The ID of the note to open.
+   * @returns None
+   */
   openNote(id: string) {
     this.router.navigateByUrl('/note?id=' + id);
   }
 
+  /**
+   * Redirects the user to the task editor page with the specified task ID.
+   * @param {string} id - The ID of the task to edit.
+   * @returns None
+   */
   editTask(id: string) {
     this.router.navigateByUrl('/task-editor?id=' + id);
   }
 
+  /**
+   * Deletes a task with the given ID.
+   * @param {string} id - The ID of the task to delete.
+   * @returns None
+   */
   deleteTask(id: string) {
     this.fileService.deleteTask(id).then(() => {
       this.fileService.readTasks().then(value => this.tasks = value);
     });
   }
 
+  /**
+   * Finds a task with the given taskid in the tasks array and checks if it has changed.
+   * @param {string} taskid - The id of the task to check.
+   * @param {string} id - The id of the task to compare with.
+   * @returns The task object if found, otherwise undefined.
+   */
   chkChanged(taskid: string, id: string) {
     var task = _.find(this.tasks, function(task) {
       return task.id == taskid;
     });
 
+    /**
+     * Updates the status of a task with the given ID and saves the updated task to the file service.
+     * @param {number} id - The ID of the task to update.
+     * @param {Task} task - The task object containing the list of tasks.
+     * @returns None
+     */
     _.each(task.tasks, function(t){
       if(t.id == id) {
         t.done = !t.done;
@@ -56,5 +124,4 @@ export class Tab2Page implements OnInit {
     })
     this.fileService.saveTask(task);
   }
-
 }
